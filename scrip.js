002ac78cee1cbbdaa799b8e962044e6fd268bf4d
lysgut <ltuninetticivarolo@escuelasproa.edu.ar>
2022-08-29 12:38:29 -0300

    el archivo scrip

---
+++ scrip.js
@@ -0,0 +1,23 @@
+function txt() {
+    let a = ['Jojo', "Sese", "Cosmik", "Loreq", "Loops", "Lecter", "Pollic"];
+    let b = Math.floor(Math.random() * a.length);
+    let c = a[b];
+    let d = document.getElementById("alien-planet").innerHTML = c; 
+}
+
+function txt2() {
+    let a = ['esternon', "kromic","esternocleido junior", "lomes","Atenea","Vegetta"];
+    let b = Math.floor(Math.random() * a.length);
+    let c = a[b];
+    let d = document.getElementById("alien-name").innerHTML = c; 
+}
+function txt3() {
+    let b = Math.floor(Math.random() * 800);
+    let d = document.getElementById("alien-age").innerHTML = b; 
+}
+
+function imga() {
+    while(1>0){
+    let a = "https://api.minimalavatars.com/avatar/random/png"
+    let b = document.getElementById("imgalien").src = a}
+}
