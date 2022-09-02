61c375a628cba5404cba4b77f3745744a41a5f82
lysgut <ltuninetticivarolo@escuelasproa.edu.ar>
2022-09-02 12:34:28 -0300

    NUEVAS TECNOLOGIAS

--- scrip.js
+++ scrip.js
@@ -1,6 +1,5 @@
 function txt() {
-    let avatar = ["https://api.minimalavatars.com/avatar/random/png",  "https://api.minimalavatars.com/avatar/random/svg"]
-    let img = avatar[Math.floor(Math.random() * avatar.length)] 
+    let avatar = "https://api.minimalavatars.com/avatar/random/svg"
     let riverarray= ["https://i.pinimg.com/736x/4b/bd/e6/4bbde6f5cde1ce085f04a264af0e38d4.jpg", "https://logodownload.org/wp-content/uploads/2015/05/river-plate-logo-0.png", "https://pre00.deviantart.net/f80f/th/pre/i/2010/250/4/8/escudo_de_river_by_raibom-d2y9803.jpg", "https://mlstaticquic-a.akamaihd.net/escudo-river-plate-argentina-en-3d-45-x-40-cm-D_NQ_NP_736060-MLU31984753367_082019-F.jpg", "http://cdn5.dibujos.net/dibujos/pintados/201703/escudo-atletico-river-plate-deportes-escudos-de-futbol-10914535.jpg", "https://4.bp.blogspot.com/-AvKpcltKlDc/WFN3pKaPQ4I/AAAAAAAACms/OaWw-PqqkCUllgRG00OjSTX6cXLUu_Y-QCLcB/s000/escudo-de-river4.jpg", "https://esteeselfamosoriver.com/wp-content/uploads/2018/11/imagenes-de-river-plate-para-fondos-de-pantalla-wallpaper-de-river-escudo-river-fondo-negro.jpg", "https://www.seekpng.com/png/full/125-1251920_escudo-png.png", "https://i.pinimg.com/originals/ae/a3/f2/aea3f20d73e6ec9a90d08fd17ee0af7c.jpg"]
     let river = riverarray[Math.floor(Math.random() * riverarray.length)];
     let ini = random(8)
@@ -39,7 +38,7 @@ function txt() {
     let descripts = [["Me gusta el futbol, suelo jugar a la pelota a diaro, de grande quiero ser jugador profesional, suelo decir SIUUU a veces, no estoy basado en JCP de la vida real y me gustan los alfajores","Hola soy Julia, me gusta leer, y limpiar, pero lo que mas me gusta es el LOL se podria decir que soy completamente adicto, lo se no estoy orgullosa","Hola soy hector y programo :D","I am the fkn president of the USA I know I ain't already and it's actually Biden but my legacy isn't died and it will have it never done","Hello I never was the president of USA but I was near. Whatever, ahora soy mexicana. Me gustan las flores, los tacos las chimichangas y trabajar duro para logrr lo que quiero","Hola soy Angelina jolie jaja si la que estaba con bad pitt esa que actua en malefica jaja si, bueno no. Soy la tatara nieta y mis viejos me pusieron mi nombre de manera honorifica, tengo una casa en la tierra y otra en marte pero mi actual residencia esta ahi en mis datos, y puedo ver a traves de las paredes porque si."],["Oi Porot so mNom ta BAse N el Nob Del Porot pq miRa s ELegum ye Porot eu NA legum","a Soy s ncomud ju comu en btasil mentie a en jksdpon qus u bras sou pqe m hsiv cord ar dec us osty","voya salilcong una prrima que prima una q bienen del campo pa basila y bebeno uno trragito mientra tanto","y quien t dio pelmiso patandando y quien tetapidiendo pelmiso mi loca te etoy absisando que y quien te dio pelmiso patandando nadie y quien te dio pelmiso patandando","aloc al ne ejautat etse etodnéimuserp ojaba atsaH 'aloip 'ogav 'ol noc ariG led ortnedA eivom al étroc el euQ ilep al óbaca el es euq eliceD xe im ed otag lA","nednopser odnum led serbil sol Y !dulas¡ ,onitnegra olbeup narg lA nednopser odnum led serbil sol Y !dulas¡ ,onitnegra olbeup narg lA !dulas¡ ,onitnegra olbeup narg lA nednopser odnum led serbil sol Y ruS led sadinU saicnivorP saL noreirba omisíngid onort us aYdadlaugi elbon al a onort le deV sanedac sator ed odiur le díO !datrebil ,datrebil ,datrebiL¡ odargas otirg le ,selatrom ,díO"],["","","","","",""],["","","","","",""],["","","","","",""],["","","","","",""],["","","","","",""],["","","","","",""],["","","","","",""]]
     let descript = descripts[ini][varnyd]
     document.getElementById("imgriver").src = river;
-    document.getElementById("imgalien").src = img;
+    document.getElementById("imgalien").src = avatar;
     document.getElementById("TITLENAME").innerHTML = "Nombre:";
     document.getElementById("alien-name").innerHTML = nombrex;
     document.getElementById("TITLEAGE").innerHTML = "Edad:"; 
@@ -59,4 +58,143 @@ function txt() {
 
 function random(x) {
     return Math.floor(Math.random() * (x + 1))
+}
+function txt2(){
+    let p1 = random(100)
+    let p2 = random(100-p1)
+    let p3 = random(100-(p1+p2))
+    let p4 = random(100-(p1+p2+p3))
+    let p5 = random(100-(p1+p2+p3+p4))
+    let p6 = random(100-(p1+p2+p3+p4+p5))
+    let p7 = random(100-(p1+p2+p3+p4+p5+p6))
+    let p8 = random(100-(p1+p2+p3+p4+p5+p6+p7))
+    let p9 = random(100-(p1+p2+p3+p4+p5+p6+p7+p8))
+    let raz = ["Humano", "Legum", "Compunivy", "Dios", "Piorodo", "Comedor de Almas", "FEx-9000", "Spacialdino", "Weruga"]
+
+
+	for (let i = raz.length - 1; i > 0; i--) {
+		let newi = random(i);
+		let aux = raz[i];
+		raz[i] = raz[newi];
+		raz[newi] = aux;
+	}
+
+    let ps = [p1,p2,p3,p4,p5,p6,p7,p8,p9]
+    for (let i = 0; i < ps.length; i++) {
+		for (let j = 0; j < ps.length; j++) {
+			if (ps[j] > ps[j + 1]) {
+				let aux = ps[j];
+				ps[j] = ps[j + 1];
+				ps[j + 1] = aux;
+			}
+		}
+	}
+    let razimg=["https://www.thoughtco.com/thmb/t4p8KVtdkuH6eLWf_c-cB-gjAZo=/2937x2937/smart/filters:no_upscale()/Zenobia-486776647x-56aa25675f9b58b7d000fd07.jpg","https://laragd.com/45-large_default/mandragora.jpg","http://2.bp.blogspot.com/_ukmdk999R0U/SsnLn4cXBTI/AAAAAAAAABQ/UgBvdN1Fj8U/s320/coloso.jpg","https://gluc.mx/u/fotografias/m/2020/8/26/f850x638-32257_109746_5050.jpg","https://wallpaperaccess.com/full/6629154.jpg","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS0BAuymH7qmtW1OEshpwxNXgz-hOtKgbxYcqDTyvVzUeQxMd5xW9fcBODnI2WfUhsHoE&usqp=CAU","https://pbs.twimg.com/media/DSndDEtXUAA9iDg.jpg","https://i.pinimg.com/originals/05/1e/f6/051ef69f88d2b77ed3636ba521bb86a8.jpg","https://preview.redd.it/l59kzfk346v61.jpg?width=640&crop=smart&auto=webp&s=9cff6d85bd7f43d7e840fef95aaa5fd57bfb3b81"]
+    let ini;
+    let ino;
+    let inu;
+    if(raz[0] == "Humano"){
+        ini = 0
+    }
+    if(raz[0] == "Legum"){
+        ini = 1
+    }
+    if(raz[0] == "Compunivy"){
+        ini = 2
+    }
+    if(raz[0] == "Dios"){
+        ini = 3
+    }
+    if(raz[0] == "Piorodo"){
+        ini = 4
+    }
+    if(raz[0] == "Comedor de Almas"){
+        ini = 5
+    }
+    if(raz[0] == "FEx-9000"){
+        ini = 6
+    }
+    if(raz[0] == "Spacialdino"){
+        ini = 7
+    }
+    if(raz[0] == "Weruga"){
+        ini = 8
+    }
+    if(raz[1] == "Humano"){
+        ino = 0
+    }
+    if(raz[1] == "Legum"){
+        ino = 1
+    }
+    if(raz[1] == "Compunivy"){
+        ino = 2
+    }
+    if(raz[0] == "Dios"){
+        ino = 3
+    }
+    if(raz[1] == "Piorodo"){
+        ino = 4
+    }
+    if(raz[1] == "Comedor de Almas"){
+        ino = 5
+    }
+    if(raz[1] == "FEx-9000"){
+        ino = 6
+    }
+    if(raz[1] == "Spacialdino"){
+        ino = 7
+    }
+    if(raz[1] == "Weruga"){
+        ino = 8
+    }
+    if(raz[2] == "Humano"){
+        inu = 0
+    }
+    if(raz[2] == "Legum"){
+        inu = 1
+    }
+    if(raz[2] == "Compunivy"){
+        inu = 2
+    }
+    if(raz[2] == "Dios"){
+        inu = 3
+    }
+    if(raz[2] == "Piorodo"){
+        inu = 4
+    }
+    if(raz[2] == "Comedor de Almas"){
+        inu = 5
+    }
+    if(raz[2] == "FEx-9000"){
+        inu = 6
+    }
+    if(raz[2] == "Spacialdino"){
+        inu = 7
+    }
+    if(raz[2] == "Weruga"){
+        inu = 8
+    }
+    document.getElementById("humano").innerHTML = ps[8] + "%"
+    document.getElementById("legum").innerHTML = ps[7] + "%"
+    document.getElementById("piorodo").innerHTML = ps[6] + "%"
+    document.getElementById("dios").innerHTML = ps[5] + "%"
+    document.getElementById("compunivy").innerHTML = ps[4] + "%"
+    document.getElementById("spacialdino").innerHTML = ps[3] + "%"
+    document.getElementById("comedor").innerHTML = ps[2] + "%"
+    document.getElementById("fex").innerHTML = ps[1] + "%"
+    document.getElementById("weruga").innerHTML = ps[0] + "%"
+    document.getElementById("h%").innerHTML =raz[0]
+    document.getElementById("l%").innerHTML =raz[1]
+    document.getElementById("p%").innerHTML =raz[2]
+    document.getElementById("d%").innerHTML =raz[3]
+    document.getElementById("c%").innerHTML =raz[4]
+    document.getElementById("s%").innerHTML =raz[5]
+    document.getElementById("co%").innerHTML =raz[6]
+    document.getElementById("f%").innerHTML =raz[7]
+    document.getElementById("w%").innerHTML =raz[8]
+    document.getElementById("imgraz").src = razimg[ini]
+    document.getElementById("imgraz2").src = razimg[ino]
+    document.getElementById("imgraz3").src = razimg[inu]
+
+    
 }
